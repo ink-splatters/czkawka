1c02158215a851c979a4f508ff0640b1f8419cb5
Peter A <ink.splatters@pm.me>
2022-08-27 02:38:19 +0200

    * flake-based shell to be invoked with `nix develop` * nix-envrc's .envrc * treefmt for rust, python, shell and md

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+
+    treefmt-flake = {
+      url = "github:srid/treefmt-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+
+    };
+  };
+
+  outputs = { self, flake-parts, nixpkgs, treefmt-flake, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+
+      imports = [ treefmt-flake.flakeModule ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+        # Provided by treefmt-flake.flakeModule
+        treefmt.formatters = {
+          inherit (pkgs)
+            shfmt
+            shellcheck
+            rustfmt
+            nixpkgs-fmt;
+          inherit (pkgs.python39Packages)
+            black
+            mdformat;
+
+        };
+        devShells = {
+
+          default = pkgs.mkShell {
+            buildInputs = (with pkgs; [
+              direnv
+              rustc
+              cargo
+              clippy
+              grcov
+              rustc.llvmPackages.llvm
+
+            ] ++ config.treefmt.buildInputs);
+
+
+            shellHook = ''
+              export PS1="\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] "
+              direnv allow .
+            '';
+
+
+          };
+
+        };
+
+
+
+
+      };
+    };
+}
--- misc/check_results.py
+++ misc/check_results.py
@@ -11,12 +11,22 @@ directories_required = sys.argv[3]
 
 file_count = sum(len(files) for _, _, files in os.walk(folder_name))
 if str(file_count) != files_required:
-    print("Current files: " + str(file_count) + ", but required is: " + str(files_required))
+    print(
+        "Current files: "
+        + str(file_count)
+        + ", but required is: "
+        + str(files_required)
+    )
     print("This commit probably introduced regression, please recheck it.")
     exit(1)
 
 directory_count = sum(len(dire) for _, dire, files in os.walk(folder_name))
 if str(directory_count) != directories_required:
-    print("Current directories: " + str(directory_count) + ", but required is: " + str(directories_required))
+    print(
+        "Current directories: "
+        + str(directory_count)
+        + ", but required is: "
+        + str(directories_required)
+    )
     print("This commit probably introduced regression, please recheck it.")
     exit(1)
